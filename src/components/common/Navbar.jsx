c9ba84dd6f71e1db77fde2379b7e6c4f81b3909a
James Edmond <jamesedmond@pursuit.org>
2023-08-29 17:48:06 -0400

    first commit

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -5,13 +5,13 @@ import "./Navbar.css"
 const Navbar = () => {
     return (
         <div className="Navbar">
-            <h1>Tou<span>Yoube</span></h1>
+            <h1>You<span>Tube</span></h1>
             <ul>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
                 <li>
-                    <Link to="/About">About Us</Link>
+                    <Link to="/About">About</Link>
                 </li>
             </ul>
         </div>
