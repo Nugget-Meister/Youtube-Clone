6adc25e3c632e56147a51e1ea88096646301d147
James Edmond <jamesedmond@pursuit.org>
2023-09-03 20:58:31 -0400

    Pictures added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from "react";
+import kc from "/src/images/kc.jpeg";
+import gl from "/src/images/gl.png";
+import je from "/src/images/je.jpg";
+//import "./About.css";
 
 const About = () => {
     return (
-        <div>
-            
+        <div className="About">
+            <div className="About-message">
+                <h2>About</h2>
+                <div className="p-wrapper">
+                    <p>
+                      Welcome,this is our YouTube clone project. Using keywords the user can search for any video similar to YouTube. This app is created by <strong>Keith Camacho</strong>,{" "} <strong>Genesis Lara</strong>, and{" "}
+                      <strong>James Edmond</strong>.
+                    </p>
+                </div>
+            </div>
+            <div>
+                <div className="card-kc">
+                    <img src={kc} />
+                    <img src={gl} />
+                    <img src={je} />
+                    
+
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./Navbar.css"
+//import "./Navbar.css"
 
 const Navbar = () => {
     return (
-        <div className="Navbar">
-            <header>
-                <h1><span>YouTube</span></h1>
-
-            </header>
+        <div className="nav-box">
+          <header>
+            <ul className="nav-bar">
+              <li className="site-title">
+                <Link to="/"><h1>YouTube</h1></Link>
+              </li>
+              <li className="list-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="list-item">
+                <Link to="/About">About</Link>
+              </li>
+            </ul>
+          </header>
         </div>
-
-    )
+      );
 }
     
 
