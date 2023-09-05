548cf7e36e817f841235bb6bab715c9cb4ce466d
Genesis Lara <genesislara@pursuit.org>
2023-09-05 17:32:13 -0400

    creates comment section form

--- src/components/Show/CommentSection.jsx
+++ src/components/Show/CommentSection.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+//onsubmit 
+// If a user leaves either the name or comment field blank, an error message should be shown and a comment should not be created.
+
 
 const CommentSection = () => {
+    
+    function handleSubmit()
+    
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input type="text" id="Name"/>
+                <input type="text" id="Comment" />
+                <input type="submit" />
+            </form>;
         </div>
     );
 }
