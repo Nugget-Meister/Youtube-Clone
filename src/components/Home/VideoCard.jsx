70cc6f32dd4872be559c33d274330a26ea7691c5
Genesis Lara <genesislara@pursuit.org>
2023-09-05 23:21:14 -0400

    small change in videocard

--- src/components/Home/VideoCard.jsx
+++ src/components/Home/VideoCard.jsx
@@ -13,7 +13,7 @@ let videoId = video.id.videoId
                 <img src= {video.snippet.thumbnails.medium.url}/>
             </Link>
             <h3>{video.snippet.title} </h3>
-            <p>channel title: {video.snippet.channelTitle} </p>
+            <p>Channel title: {video.snippet.channelTitle} </p>
         </div>
     );
 }
