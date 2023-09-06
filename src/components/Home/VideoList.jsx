7a378a4b98e2fd1d587c5e78aaad0d7b22ddd38c
Nugget-Meister <tremainecamacho@gmail.com>
2023-09-06 02:47:19 -0400

    fixed & formatted missing query

--- src/components/Home/VideoList.jsx
+++ src/components/Home/VideoList.jsx
@@ -1,13 +1,17 @@
 import React, {useState} from 'react';
 import VideoCard from './VideoCard';
-import "/src/components/Home/VideoList.css"
 
 
 const VideoList = ({ resultState }) => {
 
+    console.log(resultState.length > 0)
     return (
-        <div className="VideoList">
-            {resultState.map((item) => {return (<VideoCard video= {item}/>)})}
+        <div className="videoList">
+            {
+                resultState.length > 0 ? 
+                resultState.map((item) => {return (<VideoCard video= {item}/>)})
+                : <span className='searchpls'>Make a search to see some results!</span>
+            }
         </div>
     );
 }
