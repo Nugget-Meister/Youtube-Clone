5f6de094643006337dfc71bc72498e9d4a7bd2c6
Nugget-Meister <tremainecamacho@gmail.com>
2023-09-05 21:39:45 -0400

    error handling for searchbar and helper

--- src/components/Home/Searchbar.jsx
+++ src/components/Home/Searchbar.jsx
@@ -7,16 +7,29 @@ import { getSearchResults } from '../common/helper';
 const Searchbar = ({updateResultState}) => {
 
     const [ searchQuery, updateSearchQuery ] = useState('')
+    const [ errorFound, updateErrorFound ] = useState({
+        isError: false,
+        errorCode: 0
+    })
 
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        console.log(searchQuery)
+        // console.log(searchQuery)
         
         if(updateResultState){
             getSearchResults(searchQuery)
             .then((response) => {
-                updateResultState(response)
+                // console.log(response)
+                if(typeof response != "number") {
+                    updateResultState(response)
+                    updateErrorFound(false)
+                } else {
+                    updateErrorFound({
+                        isError: true,
+                        errorCode: response
+                    })
+                }    
             })
         } else {
             console.error("Result state has not been passed validly.")
@@ -36,6 +49,12 @@ const Searchbar = ({updateResultState}) => {
                         onChange={handleTextChange}/>
                     <input type="submit"/>
                 </form>
+                {errorFound.isError ? (
+                <div className='error'>
+                    <h1>An error has occurred!</h1>
+                    <h2>Error Code: {errorFound.errorCode}</h2>
+                </div>
+                ): null}
         </div>
     );
 }
