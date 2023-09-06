501454e6694f28f6116da9458737fd6ec1ab28f9
Nugget-Meister <tremainecamacho@gmail.com>
2023-09-05 20:20:09 -0400

    change image size for better viewing

--- src/components/Home/VideoCard.jsx
+++ src/components/Home/VideoCard.jsx
@@ -10,7 +10,7 @@ let videoId = video.id.videoId
     return (
         <div className="VideoCard">
             <Link to={`/Show/${videoId}`}>
-                <img src= {video.snippet.thumbnails.default.url}/>
+                <img src= {video.snippet.thumbnails.medium.url}/>
             </Link>
             <h3>{video.snippet.title} </h3>
             <p>channel title: {video.snippet.channelTitle} </p>
