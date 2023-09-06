2512bc24d9b741541b57cf661abb7867f3f98679
Genesis Lara <genesislara@pursuit.org>
2023-09-05 22:19:14 -0400

    completes comment section

--- src/components/Show/CommentSection.jsx
+++ src/components/Show/CommentSection.jsx
@@ -1,36 +1,82 @@
 import React, { useState } from 'react';
 
-//onsubmit 
-// If a user leaves either the name or comment field blank, an error message should be shown and a comment should not be created.
-
-
 const CommentSection = () => {
-    const [comment,setComment] = useState("")
-    const [name, setName] = useState("")
+    const [comments, setComments] = useState([])
+    const [input, setInput] = useState({
+        name: "", 
+        comment: "",
+    });
+
+    function resetForm() {
+        setInput({
+            name: "",
+            comment: "",
+        })
+    }
 
     function handleSubmit(e) {
         e.preventDefault();
-        setName (e.target.name.value)
-        setComment (e.target.comment.value)
-        console.log(comment)
-        console.log(name)
+        console.log(input)
+        setComments([
+            ...comments,
+            input,
+        ])
+        resetForm();
     }
-    
+
+    function handleTextChange(e) {
+        setInput({
+            ...input,
+            [e.target.id]: e.target.value 
+        });
+    }
+
+   
     return (
-        <div className='commentSection'>
-            <form onSubmit={handleSubmit} className='submitComment'>
-                <h3>Name:</h3>
-                <input type="text" id="name" />
-                <h3>Comment:</h3>
-                <input type="text" id="comment" />
+        <div className="commentSection">
+            <form onSubmit={handleSubmit} className="submitComment">
+                <label htmlFor="Name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={input.name}
+                    onChange={handleTextChange}
+                    required
+                />
+                < br />
+
+                <label htmlFor="comment">Comment:</label>
+                <input
+                    type="text"
+                    id="comment"
+                    value={input.comment}
+                    onChange={handleTextChange}
+                    required
+                />
+                < br />
+
                 <input type="submit" />
-            </form>;
+            </form>
+            < br />
+
+            <div className="comments">
+                {comments.map((comment) => {
+                    return (
+                        <>
+                            <h3>{comment.name}</h3>
+                            <p>{comment.comment}</p>
+                        </>)
+                })}
+
+            </div>
+
             <h3>Mo</h3>
             <p>She aight</p>
             <h3>Emily</h3>
             <p>Great video!</p>
             <h3>Bob</h3>
             <p>Who's watching this in 2030?</p>
+
         </div>
     );
 }
